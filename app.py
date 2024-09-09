17b2a43ad412f25685c8579985805edfcde5b0de
Nathan Santos <nathan_lucindosantos@hotmail.com>
2024-09-09 14:50:44 -0300

    Salvando Usuarios, e BD funcionando

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from db import db
+from models import Usuario
 
 app = Flask (__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dados.db"
+db.init_app(app)
 
-@app.route("/")
-def login_page():
-    return render_template("login_page.html")
-
-@app.route("/cadastro_page")
-def cadastro_page():
-    return render_template("cadastro_page.html")
-
+@app.route('/')
+def home():
+    usuarios = db.session.query(Usuario).all()
+    return render_template('home.html', usuarios = usuarios)
 
 
-if __name__ == "__main__":    
-    app.run(debug=True)
+@app.route('/cadastro_page', methods = ['GET', 'POST'])
+def registrar():
+    if request.method == 'GET':
+        return render_template('cadastro_page.html')
+    elif request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        telefone = request.form['telefone']
+        senha = request.form['senha']
+        novo_usuario = Usuario(nome=nome,email=email,telefone=telefone,senha=senha)
+        db.session.add(novo_usuario)
+        db.session.commit()
+        return redirect(url_for('home'))
+    
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug = True)
---
+++ models.py
@@ -0,0 +1,11 @@
+from db import db
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(150), unique = True, nullable = False)
+    email = db.Column(db.String(150), unique = True, nullable = False)
+    telefone = db.Column(db.String(150), unique = True, nullable = False)
+    senha = db.Column(db.String(20), unique = True, nullable = False)
+
+    def _repr__(self):
+        return f"<{self.nome}>"
