2c71f0c3fb4ca828b4972d9d06733624936dd97e
Nathan Santos <nathan_lucindosantos@hotmail.com>
2024-09-10 19:08:47 -0300

    tarefas funcionando

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
 from db import db
-from models import Usuario
+from models import Usuario, Tarefa
+import datetime
 
 app = Flask (__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dados.db"
@@ -41,19 +42,81 @@ def login():
         email = request.form['email']
         senha = request.form['senha']
         usuario = db.session.query(Usuario).filter_by(email=email, senha=senha).first()
-        
+
         if usuario:
-            return redirect(url_for('tarefas_page'))
+            return redirect(url_for('tarefas_page', usuario_id=usuario.id))
         else:
             return redirect(url_for('login'))
         
 # por enquanto achei melhor fazer o login com email + senha sem username
     
 
-@app.route('/tarefas_page')
-def tarefas_page():
-    return render_template('tarefas_page.html')
+@app.route('/tarefas/<int:usuario_id>', methods=['GET', 'POST'])
+def adicionar_tarefa(usuario_id):
+    usuario = db.session.query(Usuario).filter_by(id=usuario_id).first()
+    
+    if request.method == 'GET':
+        return render_template('adicionar_tarefa.html', usuario=usuario)
+    
+    elif request.method == 'POST':
+    
+        titulo = request.form['titulo']
+        data_inicio_str = request.form['data_inicio']
+        data_fim_str = request.form['data_fim']
+        descricao = request.form['descricao']
+        status = request.form['status']
+
         
+        data_inicio = datetime.datetime.strptime(data_inicio_str, '%Y-%m-%d').date()
+        data_fim = datetime.datetime.strptime(data_fim_str, '%Y-%m-%d').date()
+
+    
+        nova_tarefa = Tarefa(titulo=titulo, data_inicio=data_inicio, data_fim=data_fim,descricao=descricao,status=status,        usuario_id=usuario_id)
+        db.session.add(nova_tarefa)
+        db.session.commit()
+
+        
+        return redirect(url_for('tarefas_page', usuario_id=usuario_id))
+    
+
+@app.route('/editar_tarefa/<int:tarefa_id>', methods=['GET', 'POST'])
+def editar_tarefa(tarefa_id):
+    tarefa = db.session.query(Tarefa).filter_by(id=tarefa_id).first()
+    
+    if request.method == 'GET':
+       return render_template('editar_tarefa.html', tarefa=tarefa)
+    
+    elif request.method == 'POST':
+       
+        tarefa.titulo = request.form['titulo']
+        tarefa.data_inicio = datetime.datetime.strptime(request.form['data_inicio'], '%Y-%m-%d').date()
+        tarefa.data_fim = datetime.datetime.strptime(request.form['data_fim'], '%Y-%m-%d').date()
+        tarefa.descricao = request.form['descricao']
+        tarefa.status = request.form['status']
+
+        
+        db.session.commit()
+
+        
+        return redirect(url_for('tarefas_page', usuario_id=tarefa.usuario_id))
+
+
+
+@app.route('/tarefas_page/<int:usuario_id>', methods=['GET'])
+def tarefas_page(usuario_id):
+    usuario = db.session.query(Usuario).filter_by(id=usuario_id).first()
+
+   
+    status_filtro = request.args.get('status')
+
+    if status_filtro:
+       
+        tarefas = db.session.query(Tarefa).filter_by(usuario_id=usuario_id, status=status_filtro).all()
+    else:
+       
+        tarefas = db.session.query(Tarefa).filter_by(usuario_id=usuario_id).all()
+
+    return render_template('tarefas_page.html', usuario=usuario, tarefas=tarefas)
 
 
 
--- models.py
+++ models.py
@@ -5,7 +5,18 @@ class Usuario(db.Model):
     nome = db.Column(db.String(150), unique = True, nullable = False)
     email = db.Column(db.String(150), unique = True, nullable = False)
     telefone = db.Column(db.String(150), unique = True, nullable = False)
-    senha = db.Column(db.String(20), unique = True, nullable = False)
+    senha = db.Column(db.String(20), unique = False, nullable = False)
 
     def _repr__(self):
         return f"<{self.nome}>"
+    
+
+class Tarefa(db.Model):
+    __tablename__ = 'tarefas'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    titulo = db.Column(db.String(150), nullable=False)
+    data_inicio = db.Column(db.Date, nullable=False)
+    data_fim = db.Column(db.Date, nullable=False)
+    descricao = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False) 
